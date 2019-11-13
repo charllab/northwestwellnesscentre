b29e7824cc0bfb45fbde14e473868c4537848878
charllab <developer.charl@gmail.com>
2019-11-13 14:50:46 -0800

    started banner block with positioning

--- src/themes/northwestwellnesscentre/front-page.php
+++ src/themes/northwestwellnesscentre/front-page.php
@@ -1,3 +1,67 @@
 <?php get_header(); ?>
 
+    <main>
+        <section class="bg-warning">
+            <div class="container py-4 py-md-7">
+                <div class="row">
+                    <div class="col col-sm-8 offset-sm-2 col-lg-6 offset-lg-3">
+                        <h2 class="h1 text-white text-center mb-0">Balanced health for your mind, body and spirit.</h2>
+                        <div class="d-sm-flex justify-content-center mt-150">
+                            <a href="#" class="btn btn-primary btn-xs-block mb-1 mb-sm-0 mx-sm-50">Our Services</a>
+                            <a href="#" class="btn btn-secondary btn-xs-block mx-sm-50">Watch Video</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <br>
+        <br>
+
+        <section class="bg-warning">
+            <div class="container py-4 py-md-7">
+                <div class="row">
+                    <div class="col col-sm-8 offset-sm-2 col-lg-4 offset-lg-8">
+                        <h2 class="h1 text-center text-md-left text-white mb-0 mx-50">Child & Family Therapy Counseling</h2>
+                        <div class="d-sm-flex mt-150">
+                            <a href="#" class="btn btn-primary btn-xs-block mb-1 mb-sm-0 mx-sm-50">Our Services</a>
+                            <a href="#" class="btn btn-secondary btn-xs-block mx-sm-50">Watch Video</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <br>
+        <br>
+
+        <section class="bg-warning">
+            <div class="container py-4 py-md-7">
+                <div class="row">
+                    <div class="col col-sm-8 offset-sm-2 col-lg-4 offset-lg-0">
+                        <h2 class="h1 text-center text-md-left text-white mb-0 mx-50">Child & Family Therapy Counseling</h2>
+                        <div class="d-sm-flex mt-150">
+                            <a href="#" class="btn btn-primary btn-xs-block mb-1 mb-sm-0 mx-sm-50">Our Services</a>
+                            <a href="#" class="btn btn-secondary btn-xs-block mx-sm-50">Watch Video</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section>
+            <div class="container py-2">
+                <div class="row justify-content-center">
+                    <div class="col col-sm-8 text-center">
+                        <h2 class="h1">Health and Wellness Services in Grande Prairie and Grimshaw Alberta</h2>
+                        <p>Our experienced practitioners offer a complete range of health services including individualized care plans that promote optimal healing of the mind, body and spirit.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+
 <?php get_footer();
