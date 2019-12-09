7102d10fd0e8c29f8424fd27e9c46b683584ebee
charllab <developer.charl@gmail.com>
2019-12-09 14:46:26 -0800

    Monday push

--- src/themes/northwestwellnesscentre/page-contact.php
+++ src/themes/northwestwellnesscentre/page-contact.php
@@ -27,45 +27,80 @@ get_header(); ?>
 
                         <?php endif; ?>
 
+                        <?php echo do_shortcode('[gravityform id=1 title=false description=false ajax=true tabindex=10]'); ?>
+
                     </div><!-- col -->
 
+
                     <div class="col-lg-5">
-                        <div class="pt-3 pb-2 px-2 bg-light">
+                        <div class="p-1 pt-md-3 pb-md-2 px-md-2 bg-light">
                             <h2>Contact Information</h2>
-                            <?php
-                            $removethese = array("(", " ", ")", "-");
-                            ?>
-                            <table>
-                                <tr>
-                                    <td><strong>Phone: </strong></td>
-                                    <td>
-                                        <a href="tel:+1<?php echo strip_tel(get_field('primary_number', 'option')); ?>"><?php echo get_field('primary_number', 'option'); ?></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="pr-1"><strong>Toll Free: </strong></td>
-                                    <td>
-                                        <a href="tel:+1<?php echo strip_tel(get_field('secondary_number', 'option')); ?>"><?php echo get_field('secondary_number', 'option'); ?></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><strong>E-mail: </strong></td>
-                                    <td>
-                                        <a href="mailto:<?php echo get_field('primary_email', 'option'); ?>"><?php echo get_field('primary_email', 'option'); ?></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><strong>Address: </strong></td>
-                                    <td><?php echo get_field('physical_address', 'option'); ?></td>
-                                </tr>
-                            </table>
+
+
+                            <div class="row">
+                                <?php
+                                $removethese = array("(", " ", ")", "-");
+                                ?>
+
+
+                                <?php if (have_rows('location', 'option')): ?>
+
+
+                                    <?php while (have_rows('location', 'option')): the_row();
+
+                                        // vars
+                                        $otherwebsite = get_sub_field('other_website');
+
+                                        ?>
+                                        <div class="col-md-6 col-lg-12 location__info--repeater-col">
+
+                                            <h5 class="location__info--header"><?php echo get_sub_field('location_name', 'option'); ?></h5>
+
+                                            <table class="location__info">
+                                                <tr>
+                                                    <td class="location__info--td"><strong>Phone: </strong></td>
+                                                    <td>
+                                                        <a href="tel:+1<?php echo strip_tel(get_field('phone_number', 'option')); ?>"><?php echo get_sub_field('phone_number', 'option'); ?></a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>Address: </strong></td>
+                                                    <td><?php echo get_sub_field('physical_address', 'option'); ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>Hours: </strong></td>
+                                                    <td><p class="mb-50 "><?php echo get_sub_field('trading_hours', 'option'); ?></p>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="location__info--blanks">&nbsp;</td>
+                                                    <td>
+                                                        <a
+                                                            href="<?php echo get_sub_field('map_embed_code', 'option'); ?>"
+                                                            target="_blank"><strong>Get Directions</strong> <i
+                                                                class="fas fa-external-link-alt"></i></a></td>
+                                                </tr>
+                                                <?php if ($otherwebsite): ?>
+                                                    <tr>
+                                                        <td class="location__info--blanks">&nbsp;</td>
+                                                        <td>
+                                                            <a href="mailto:<?php echo get_field('other_website', 'option'); ?>" class="d-inline-block mt-md-50">
+                                                                <strong>Visit <?php echo get_sub_field('location_name', 'option'); ?>
+                                                                    website</strong> <i
+                                                                    class="fas fa-external-link-alt"></i></a>
+                                                        </td>
+                                                    </tr>
+                                                <?php endif; ?>
+                                            </table>
+                                        </div>
+                                    <?php endwhile; ?>
+
+                                <?php endif; ?>
+
+
+                            </div>
                         </div><!-- bg-light -->
 
-                        <div class="px-0">
-                            <?php
-                            echo get_field('map_embed_code', 'option');
-                            ?>
-                        </div><!-- px-0 -->
                     </div><!-- col -->
                 </div><!-- row -->
 
